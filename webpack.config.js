05c2f3589c3653ce6fad388af3a93f21808b88df
AnatolyKyznetsov <120422@bk.ru>
2024-08-16 13:49:18 +0700

    webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,45 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ZipPlugin = require('zip-webpack-plugin');
 
-module.exports = () => {
-    return {
-        mode: 'production',
-        entry: './src/index.js',
-        module: {
-            rules: [
+const config = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
                 {
-                    test: /\.css$/i,
-                    use: ['style-loader', 'css-loader'],
-                },
-            ],
-        },
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'index.js',
-        },
-        plugins: [
-            new CopyWebpackPlugin({
-                patterns: [
-                    {
-                        from: path.resolve(__dirname, 'static'),
-                        to: path.resolve(__dirname, 'dist')
-                    }
-                ]
+                    from: path.resolve(__dirname, 'static'),
+                    to: path.resolve(__dirname, 'dist')
+                }
+            ]
+        })
+    ]
+}
+
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') {
+        config.devtool = 'source-map';
+    } else if (argv.mode === 'production') {
+        config.plugins.push(
+            new ZipPlugin({
+                path: '../release/',
+                filename: 'jira_extention.zip',
+                pathPrefix: 'jira_extention',
             })
-        ]
+        );
     }
+
+    return config;
 };
